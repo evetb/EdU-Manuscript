67c4b44442289f7836d38a9601a8f18dc09d1f73
evetb <47900347+evetb@users.noreply.github.com>
2022-07-19 11:48:55 -0400

    Made comments more tidy

--- Abx_Experiment/16S/rarefaction_curves.py
+++ Abx_Experiment/16S/rarefaction_curves.py
@@ -12,21 +12,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Messing with font as per https://stackoverflow.com/questions/20753782/default-fonts-in-seaborn-statistical-data-visualization-in-ipython
-# and per https://github.com/matplotlib/matplotlib/issues/17568
-# and https://linuxtut.com/en/791e3cd7f75e010c8f9f/
-#import matplotlib as mpl
-#font_paths = mpl.font_manager.findSystemFonts()
-# 'C:\\WINDOWS\\Fonts\\times.ttf'
-#font_objects = mpl.font_manager.createFontList(font_paths)
-#for font_path in font_paths:
-#    mpl.font_manager.fontManager.addfont(font_path)
-#font_names = [f.name for f in font_objects]
-#print(font_names)
-
 # Formatting the csv file for plotting
 path = 'C:/Users/etb/Documents/McGill/Experiments/Abx/abx1_2_controls/revised_diversity/alpha/nsnc-otus-rare.csv'
-#path = 'C:/Users/etb/Documents/McGill/Experiments/Abx/abx_2/revised_diversity/alpha/nsnc-otus-rare.csv'
 otus = pd.read_csv(path,index_col=0, sep=',')
 
 cols = [col for col in otus.columns if 'iter' not in col]
@@ -44,10 +31,6 @@ pal={'Control':'blue','Initial':'green'}
 #pal={'Control':'blue','Initial':'green','Van':'red','Pmb':'black'}
 fig,ax = plt.subplots(figsize=(2.5,2),dpi=600,tight_layout=True)
 sns.set(style='ticks',rc={"lines.linewidth":0.7,"axes.linewidth":0.5})
-# the above only made the font on the inside of the graph in TNR lol
-# whatever, dropping the issue for now (28 April 2022). not worth my time
-# somehow adding & then removing the font='Times New Roman' prompt ended up
-# making all fonts OUTSIDE the plot (but not inside) as bold TNR??
 
 # use your column name to plot here instead of 'treatment'
 sns.lineplot(x='depth',y='ASV',data=data,palette=pal,hue='treatment',sort=False,err_style='bars',\
